7b33b0150e379950f1399a403c15f73c10c5acc5
Dani <yo2@yo>
2022-02-18 13:40:58 +0100

    patata commit

--- prueba/src/prueba/Nueva.java
+++ prueba/src/prueba/Nueva.java
@@ -4,12 +4,9 @@ public class Nueva {
 public static void main(String[] args) {
 	System.out.println("a");
 	System.out.println("b");
-<<<<<<< HEAD
 	System.out.println("c");
 	System.out.println("d");
-}
-=======
 	System.out.println("hola");
+	System.out.println("patata");
 	}
->>>>>>> branch 'master' of git@github.com:dasnib/repo11-02.git
 }
